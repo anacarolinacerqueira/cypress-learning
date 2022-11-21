40c18e3070d527d9f4b0acf540e3d19e3ec23586
root <cf.anacarolina@gmail.com>
2022-11-21 20:42:23 -0300

    cria estrutura de pastas e primeiro teste de exemplo

---
+++ buger-eats/cypress/e2e/home.spec.cy.js
@@ -0,0 +1,13 @@
+// 'describe' é a palavra reservada que define o nome da suíte de testes no cypress
+describe('home page', ()=>{
+    //'it' é a palavra reservada que define o nome do caso de teste
+    it('Validar que página esteja online', ()=>{
+        //'cy.viewport' define a resolução da tela na qual o teste será executado
+        cy.viewport(1440, 900)
+        //'cy.visit' é utilizado para definir o endereço que deve ser acessado
+        cy.visit('http://buger-eats.vercel.app')
+        //'cy.get' busca por elemento determinado na página
+        //'.should é a função que realiza o "assert", passando os argumentos 'have.text', e em seguida o valor que deve estar no campo
+        cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+    })
+})
