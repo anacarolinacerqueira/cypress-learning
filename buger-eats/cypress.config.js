609a302618e37e8031d57c5dd1b98eec7f6aba2b
root <cf.anacarolina@gmail.com>
2022-11-26 19:39:49 -0300

    adicionar projectId para integração com cypress dashboard

--- buger-eats/cypress.config.js
+++ buger-eats/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    projectId: "wp11gg",
     viewportWidth: 1366,
     viewportHeight: 768,
     baseUrl:'https://buger-eats.vercel.app',
